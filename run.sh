6909fa70474862217f2aa83b108d15dfc0b5ebb3
Jannatin Naim <man@jannatinnaim.com>
2026-05-20 10:20:13 +0600

    update run script and add clang-format

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-clang -std=c89 -Wall -Wextra -Wpedantic -Wshadow -Wconversion -O0 -g -c main.c && clang main.o -o main && rm main.o && (./main; STATUS=$?; rm main; exit $STATUS)
+command -v clang-format >/dev/null 2>&1 && \
+clang-format -i -style="{BasedOnStyle: LLVM, BreakBeforeBraces: Allman, IndentWidth: 4, KeepEmptyLinesAtTheStartOfBlocks: false}" main.c
+
+clang -std=c89 -Wall -Wextra -Wpedantic -Wshadow -Wconversion -O0 -g -c main.c && \
+clang main.o -o main && \
+rm main.o && \
+(./main; STATUS=$?; rm main; exit $STATUS)
