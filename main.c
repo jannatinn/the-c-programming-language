0860646245d499aa1c3b6b3c7ab3057725b266a0
Jannatin Naim <man@jannatinnaim.com>
2026-05-20 09:23:02 +0600

    exercise 1.1

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+main()
 {
-    int i;
-
-    printf("argc: %d\n", argc);
-    for (i = 0; i < argc; i++)
-    {
-        printf("argv %d: %s\n", i, argv[i]);
-    }
-
-    printf("Hello, world!\n");
-
-    return EXIT_SUCCESS;
+    printf("hello, world\n");
 }
