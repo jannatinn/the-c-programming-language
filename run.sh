4481ad44ffa682a3928c6ed27a322d3794e4c972
Jannatin Naim <man@jannatinnaim.com>
2026-05-20 23:09:20 +0600

    update run configuration

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ clang-format -i -style="{BasedOnStyle: LLVM, BreakBeforeBraces: Allman, IndentWi
 clang -std=c89 -Wall -Wextra -Wpedantic -Wshadow -Wconversion -O0 -g -c main.c && \
 clang main.o -o main && \
 rm main.o && \
-(./main; STATUS=$?; rm main; exit $STATUS)
+./main; \
+rm main
