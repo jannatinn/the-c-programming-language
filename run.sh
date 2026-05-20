fff5a8062897ba38bdee4d26fd50d500829941e1
Jannatin Naim <man@jannatinnaim.com>
2026-05-20 09:31:14 +0600

    update run configuration

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-clang -std=c89 -Wall -Wextra -Wpedantic -Wshadow -Wconversion -O0 -g -c main.c && clang main.o -o main && rm main.o && ./main
+clang -std=c89 -Wall -Wextra -Wpedantic -Wshadow -Wconversion -O0 -g -c main.c && clang main.o -o main && rm main.o && (./main; STATUS=$?; rm main; exit $STATUS)
