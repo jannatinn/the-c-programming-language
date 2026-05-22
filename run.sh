143de31905a0c16829298465e64a3d92eeaa2647
Jannatin Naim <man@jannatinnaim.com>
2026-05-23 05:30:43 +0600

    update tooling

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-command -v clang-format >/dev/null 2>&1 && clang-format -i -style=file main.c
-clang $(tr '\n' ' ' < compile_flags.txt) -O0 -g -c main.c && clang main.o -o main && rm main.o && ./main; rm main
+command -v clang-format >/dev/null 2>&1 && clang-format -i -style=microsoft main.c
+clang $(tr '\n' ' ' < compile_flags.txt) -O0 -g -c main.c -o main && ./main; rm main; rm main.dSYM
