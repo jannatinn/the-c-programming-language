714c706934a58b32c18135fd0080551ca91b513c
Jannatin Naim <man@jannatinnaim.com>
2026-05-23 05:48:16 +0600

    update build command

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 command -v clang-format >/dev/null 2>&1 && clang-format -i -style=microsoft main.c
-clang $(tr '\n' ' ' < compile_flags.txt) -O0 -g -c main.c -o main && ./main; rm main; rm main.dSYM
+clang $(tr '\n' ' ' < compile_flags.txt) -O0 -g -o main main.c && ./main; rm -rf main main.dSYM
