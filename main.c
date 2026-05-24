b86e6fafe1be9914dc60dbae00a5ac4f1ac707ed
Jannatin Naim <man@jannatinnaim.com>
2026-05-25 00:55:41 +0600

    fahrenheit to celsius table

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+/* fahrenheit to celsius table */
 main()
 {
-    printf("hello, world\8");
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
