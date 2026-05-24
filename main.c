a7ed889d2bfe6cf33328b45e800e4002383c500d
Jannatin Naim <man@jannatinnaim.com>
2026-05-25 01:23:08 +0600

    fahrenheit to celsius table with header

--- main.c
+++ main.c
@@ -10,13 +10,13 @@ main()
     upper = 300;
     step = 20;
 
+    printf("fahr => celsius\n");
+
     fahr = lower;
     while (fahr <= upper)
     {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        /* digit before f is "atleast x spaces"; uses space */
-        /* digit after . is "simply x spaces"; uses 0 */
-        printf("%.2f\t%6f\n", fahr, celsius);
+        printf("%4.0f\t%7.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
