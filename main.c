3cbb01bde3b27c752f39b28e08425b9edd38528c
Jannatin Naim <man@jannatinnaim.com>
2026-05-25 01:07:51 +0600

    integer division truncation

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 /* fahrenheit to celsius table */
 main()
 {
-    int i, j;
-
     int fahr, celsius;
     int lower, upper, step;
 
@@ -15,13 +13,9 @@ main()
     fahr = lower;
     while (fahr <= upper)
     {
-        celsius = 5 * (fahr - 32) / 9;
+        /* celsius = 5 * (fahr - 32) / 9; */
+        celsius = 5 / 9 * (fahr - 32);
         printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
-
-    i = 1;
-    j = 9;
-    while (i < j)
-        i = 2 * i;
 }
